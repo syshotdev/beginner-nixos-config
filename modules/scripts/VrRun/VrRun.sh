f904e9f4b08da7a33e3e02ff7f38f0e1805b911e
syshotdev <syshotdev@gmail.com>
2024-09-28 11:31:59 -0700

    Exported variables to make vr work

--- modules/scripts/VrRun/VrRun.sh
+++ modules/scripts/VrRun/VrRun.sh
@@ -2,8 +2,8 @@
 # This script basically sets some environment variables to vr run better 
 # Requires steam to be installed
 
-VRCLIENT="~/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrclient.so"
-STOREPATH=$(nix-store -qR $(which steam) | grep steam-fhs)/lib64
+export VRCLIENT="~/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrclient.so"
+export STOREPATH=$(nix-store -qR $(which steam) | grep steam-fhs)/lib64
 
 # Patch the VR client
 echo "Patching VR client at $VRCLIENT with rpath $STOREPATH"
