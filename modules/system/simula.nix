e0c306f27816361bce26fdabc6cdb587c150c840
syshotdev <syshotdev@gmail.com>
2024-11-05 21:11:01 -0800

    Finally resolved all of the conflicts, nixos vs nixos-config

---
+++ modules/system/simula.nix
@@ -0,0 +1,14 @@
+/*
+# Clone
+git clone --depth 1 --recursive https://github.com/SimulaVR/Simula
+cd Simula
+
+# Download (or build, if downloading fails)
+source ./utils/Helpers.sh && installSimula
+
+# Launch
+./result/bin/simula # Needs SteamVR to be running
+*/
+
+
+
