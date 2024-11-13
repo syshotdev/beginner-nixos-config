b2e6769b44a5f9cb277d6f4c84eea8961e2ce101
syshotdev <syshotdev@gmail.com>
2024-11-12 23:42:23 -0800

    Refactored scripts to be simpler

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -1,7 +1,25 @@
-{
-  # Write script named "find-banned-in-files" to path
-  find-banned-in-files = ./FindBannedInFiles;
-  publicize-folder = ./PublicizeFolder;
-  rebuild-system = ./RebuildSystem;
-  vr-run = ./VrRun;
+{ pkgs, ... }:
+let
+  # Helper function for making commands that you can execute via terminal
+  mkScript = name: scriptPath: extraPkgs: pkgs.writeScriptBin name 
+    (builtins.readFile scriptPath) {
+      # Fundamental packages for execution
+      coreutils = pkgs.coreutils;
+      bash = pkgs.bash;
+
+      # Extra packages required by script
+      inherit extraPkgs;
+    };
+
+  # Define your scripts here
+  scripts = [
+    # TODO: Find a way to add bannedwords.txt to the directory of FindBannedInFiles.sh
+    (mkScript "find-banned-in-files" ./FindBannedInFiles.sh [ pkgs.ripgrep ])
+    (mkScript "publicize-folder" ./PublicizeFolder.sh [])
+    (mkScript "rebuild" ./RebuildSystem.sh [])
+    (mkScript "vr-run" ./VrRun.sh [ pkgs.patchelf ])
+  ];
+in {
+  # scripts = scripts, to access put scriptModules.scripts in imports and it'll install all of 'em
+  inherit scripts;
 }
