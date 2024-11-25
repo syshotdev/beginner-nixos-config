431cc2197fa48241eebac7f125c9d69d2384a226
syshotdev <syshotdev@gmail.com>
2024-11-24 16:27:32 -0800

    Added override for nvidia gpu to hopefully quell error messages

--- modules/system/optimizations/gpu/nvidia/default.nix
+++ modules/system/optimizations/gpu/nvidia/default.nix
@@ -11,21 +11,9 @@
     powerManagement.finegrained = false;
   };
 
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
-  /*let 
-  rcu_patch = pkgs.fetchpatch {
-    url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-    hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-  };
-  in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "550.40.07";
-    sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-    sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-    openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-    settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-    persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
+  # Some guy says this will fix "DRM kernel driver 'nvida-drm in use. NVK requires nouveau"
+  boot.kernelParams = [ "nvidia-drm.fbdev=1" ];
 
-    patches = [ rcu_patch ];
- };*/
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
 }
 
