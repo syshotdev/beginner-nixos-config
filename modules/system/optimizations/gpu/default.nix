726603478b2553bfa5d1f0e60e824cc7d1a4e303
syshotdev <syshotdev@gmail.com>
2024-11-24 16:54:59 -0800

    Added setLdLibraryPath override. Maybe remove errors?

--- modules/system/optimizations/gpu/default.nix
+++ modules/system/optimizations/gpu/default.nix
@@ -4,6 +4,8 @@
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
+    # https://discourse.nixos.org/t/getting-an-error-has-anything-regarding-opengl-in-nixpkgs/3641/3
+    setLdLibraryPath = true;
     # Mesa is general opengl drivers (I think)
     extraPackages = [ pkgs.mesa.drivers ];
   };
